c20c115a86cc94c9326ac59d8fed4f4c24fb7344
BlazZupan <blaz.zupan@fri.uni-lj.si>
2016-06-24 17:31:26 +0200

    Embedding library and widget.

---
+++ orangecontrib/imageanalytics/embeddimage.py
@@ -0,0 +1,121 @@
+import os
+import os.path
+import numpy as np
+import pickle
+
+from PIL import Image
+import io
+import base64
+import hashlib
+import Orange.misc.environ
+import requests
+from requests.exceptions import ConnectionError
+
+
+class ImageProfiler:
+    ENCODING = "utf-8"
+    THUMBNAIL_SIZE = (299, 299)
+    PICKLE_FILE = Orange.misc.environ.cache_dir() + "/image_embeddings.pkl"
+
+    def __init__(self, server=None, token=None, clear_history=False):
+        if os.path.exists(self.PICKLE_FILE) and not clear_history:
+            self.history = pickle.load(open(self.PICKLE_FILE, "rb"))
+        else:
+            self.history = {}
+
+        self.server = server if server else self.get_server_address()
+        self.coins = 0
+        self.token = ""
+        if token:
+            self.set_token(token)
+        # todo get server address, report if fails (e.g., no network)
+
+    def __enter__(self):
+        return self
+
+    def set_token(self, token):
+        if self.server and self.check_token_valid(token):
+            self.token = token
+            self.coins = self.get_coin_count()
+        else:
+            self.token = ""
+
+    def get_server_address(self):
+        url = "https://raw.githubusercontent.com/biolab/" \
+              "orange3-imageanalytics/master/SERVERS.txt"
+        try:
+            res = requests.get(url)
+        except ConnectionError:
+            return None
+        server = res.text.strip().split("\n")[0]
+        if self.check_server_alive(server):
+            return server
+        else:
+            return None
+
+    def check_server_alive(self, server):
+        try:
+            r = requests.head(server + "/info")
+            status = r.status_code
+            alive = status == 200
+            return alive
+        except ConnectionError:
+            return False
+
+    @staticmethod
+    def compute_hash(obj):
+        return hashlib.sha224(obj).hexdigest()
+
+    def get_coin_count(self):
+        json = {"token": self.token}
+        res = requests.post(self.server + "coin_count", json=json)
+        return res.json().get("coins")
+
+    def check_token_valid(self, token):
+        json = {"token": token}
+        res = requests.post(self.server + "check_token_valid", json=json)
+        return res.json().get("valid")
+
+    def __call__(self, image_file_name):
+        im = Image.open(image_file_name)
+        im.thumbnail(self.THUMBNAIL_SIZE, Image.ANTIALIAS)
+        out = io.BytesIO()
+        im.save(out, format="JPEG")
+        base64_bytes = base64.b64encode(out.getbuffer())
+
+        h = self.compute_hash(base64_bytes)
+        if h in self.history:
+            return self.history[h]
+
+        base64_string = base64_bytes.decode(self.ENCODING)
+        json = {"image": base64_string, "token": self.token}
+        res = requests.post(self.server + "image_profiler", json=json)
+        if not res.json()["profile"]:
+            return None
+        profile = np.array(res.json()["profile"], dtype=np.float16)
+        self.history[h] = profile
+        self.coins -= 1
+        return profile
+
+    def dump_history(self):
+        pickle.dump(self.history, open(self.PICKLE_FILE, "wb"), -1)
+
+    def __exit__(self, *_):
+        self.dump_history()
+
+if __name__ == "__main__":
+    with ImageProfiler(token="483ea629-0a85-4390-a494-58cfd8324a82",
+                       clear_history=True) \
+            as image_profiler:
+        print("Server:", image_profiler.server)
+        result = image_profiler("example-image.jpg")
+        print(result)
+
+    # with ImageProfiler(server="http://193.2.72.53:8080/") \
+    #         as image_profiler:
+    #     print("Server:", image_profiler.server)
+
+    # with ImageProfiler(server="http://193.2.72.53:8080/") \
+    #         as image_profiler:
+    #     result = image_profiler("example-image.jpg")
+    #     print(result)
---
+++ orangecontrib/imageanalytics/widgets/owimagenetembedding.py
@@ -0,0 +1,145 @@
+import numpy as np
+
+from Orange.widgets import widget, gui
+from Orange.widgets.settings import Setting
+from Orange.data import Table, Domain, ContinuousVariable
+from orangecontrib.imageanalytics.embeddimage import ImageProfiler
+from PyQt4 import QtGui
+from PyQt4.QtGui import QDesktopServices
+from PyQt4.QtCore import Qt, QUrl
+
+
+class OWImageNetEmbedding(widget.OWWidget):
+    name = "ImageNet Embedding"
+    description = "Image profiling through deep network from ImageNet."
+    icon = "icons/ImageNetEmbedding.svg"
+    priority = 2000
+
+    inputs = [("Data", Table, "set_data")]
+    outputs = [("Embeddings", Table, widget.Default)]
+    auto_commit = Setting(True)
+    token = Setting("")
+
+    want_main_area = False
+
+    def __init__(self):
+        super().__init__()
+
+        self.data = None
+        self.file_att = None  # attribute with image filenames
+        self.origin = None  # origin of image files
+
+        box = gui.widgetBox(self.controlArea, "Info")
+        self.info_a = gui.widgetLabel(box, "No data on input.")
+        self.info_b = gui.widgetLabel(box, "")
+
+        box = gui.widgetBox(self.controlArea, "Server Token")
+        gui.lineEdit(box, self, "token", "Token: ",
+                     controlWidth=250, orientation=Qt.Horizontal,
+                     enterPlaceholder=True, callback=self.token_name_changed)
+
+        gui.button(
+            box, self, "Get Token",
+            callback=self.open_server_token_page,
+            autoDefault=False
+        )
+
+        self.controlArea.setMinimumWidth(self.controlArea.sizeHint().width())
+        self.layout().setSizeConstraint(QtGui.QLayout.SetFixedSize)
+
+        self.commit_box = gui.auto_commit(
+            self.controlArea, self, "auto_commit", label="Commit",
+            checkbox_label="Commit on change"
+        )
+        self.commit_box.setEnabled(False)
+
+        self.profiler = ImageProfiler(token=self.token, clear_history=True)
+        if self.token:
+            self.profiler.set_token(self.token)
+        self.set_info()
+
+    def set_info(self):
+        if self.profiler.token:
+            if self.profiler.server:
+                self.info_b.setText("Server ok. Image credit:"
+                                    " {}".format(self.profiler.coins))
+                if self.profiler.coins > 0:
+                    self.commit_box.setEnabled(True)
+                    self.warning(0, "")
+            else:
+                self.info_b.setText("Connection with server not established.")
+        else:
+            self.info_b.setText("Please enter valid server token.")
+
+    def open_server_token_page(self):
+        url = QUrl(self.profiler.server + "get_token")
+        QDesktopServices.openUrl(url)
+
+    def set_data(self, data):
+        if data is None:
+            self.send("Data", None)
+            self.info_a.setText("No data on input.")
+            return
+
+        self.info_a.setText("Data with %d instances." % len(data))
+        self.data = data
+        # todo check for image attributes
+
+        atts = [a for a in data.domain.metas if
+                a.attributes.get("type") == "image"]
+        if not atts:
+            self.warning(text="Input data has no image attributes.")
+            self.info_a.setText("Data (%d instances) without image "
+                                "attributes." % len(data))
+            self.data = None
+            return
+        self.file_att = atts[0]  # todo handle a set of image attributes
+        self.origin = self.file_att.attributes.get("origin", "")
+        if self.auto_commit and self.profiler.coins >= 0:
+            self.commit()
+
+    def token_name_changed(self):
+        self.profiler.set_token(self.token)
+        self.set_info()
+
+    def commit(self):
+        if len(self.data) > self.profiler.coins:
+            self.warning(0, "Not enough credit to process images.")
+            return
+        with self.progressBar(len(self.data)) as progress:
+            if not self.profiler.server:
+                self.info_b.setText("Embedding server not available.")
+                self.send("Embeddings", None)
+                return
+            xp = []
+            for d in self.data:
+                filename = self.origin + "/" + str(d[self.file_att])
+                ps = self.profiler(filename)
+                xp.append(ps)
+                progress.advance()
+
+        xp = np.array(xp)
+        x = np.hstack((self.data.X, xp))
+        atts = [ContinuousVariable("n%d" % i) for i in
+                range(xp.shape[1])]
+        domain = Domain(list(self.data.domain.attributes) + atts,
+                        self.data.domain.class_vars,
+                        self.data.domain.metas)
+        embeddings = Table(domain, x, self.data.Y, self.data.metas)
+        self.send("Embeddings", embeddings)
+        self.set_info()
+
+
+if __name__ == "__main__":
+    import sys
+    from PyQt4.QtGui import QApplication
+
+    app = QApplication(sys.argv)
+    ow = OWImageNetEmbedding()
+    origin = "/Users/blaz/Desktop/image-small"
+    data = Table(origin + "/data-small.tab")
+    data.domain["img"].attributes.update({"origin": origin})
+    ow.set_data(data)
+    ow.show()
+    app.exec()
+    ow.saveSettings()

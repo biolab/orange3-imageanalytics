c1bd8be30d58586c4f1150632cd98da8d96e86a8
inejc <nejc.ilenic@gmail.com>
2017-03-26 20:00:09 +0200

    Http2Client: check for environment variables when setting server url and port

--- orangecontrib/imageanalytics/tests/test_image_embedder.py
+++ orangecontrib/imageanalytics/tests/test_image_embedder.py
@@ -2,6 +2,7 @@ import json
 import logging
 import unittest
 from io import BytesIO
+from os import environ
 from os.path import join, dirname
 from unittest.mock import patch
 
@@ -240,3 +241,26 @@ class ImageEmbedderTest(unittest.TestCase):
         res = self.embedder([_EXAMPLE_IMAGE_TIFF])
         assert_array_equal(res, np.array([np.array([0, 1], dtype=np.float16)]))
         self.assertEqual(len(self.embedder._cache_dict), 1)
+
+    @patch(_TESTED_MODULE.format('HTTP20Connection'), DummyHttp2Connection)
+    def test_server_url_port_env_vars(self):
+        url_value = 'url'
+        port_value = '1234'
+
+        self.assertTrue(self.embedder._server_url != url_value)
+        self.assertTrue(self.embedder._server_port != int(port_value))
+
+        environ['ORANGE_EMBEDDING_API_URL'] = url_value
+        environ['ORANGE_EMBEDDING_API_PORT'] = port_value
+        self.embedder = ImageEmbedder(
+            model='inception-v3',
+            layer='penultimate',
+            server_url='example.com',
+            server_port=80
+        )
+
+        self.assertTrue(self.embedder._server_url == url_value)
+        self.assertTrue(self.embedder._server_port == int(port_value))
+
+        del environ['ORANGE_EMBEDDING_API_URL']
+        del environ['ORANGE_EMBEDDING_API_PORT']

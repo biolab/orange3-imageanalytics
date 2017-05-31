6d87393c302c10e568ee7028b2e33d496bf7c90e
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2017-05-31 11:25:28 +0200

    tests: Correct package  top_level_dir in test discovery

--- orangecontrib/imageanalytics/widgets/tests/__init__.py
+++ orangecontrib/imageanalytics/widgets/tests/__init__.py
@@ -5,5 +5,9 @@ import unittest
 def load_tests(loader, tests, pattern):
     """Loads this module's tests."""
     module_dir = os.path.dirname(__file__)
-    module_tests = loader.discover(module_dir, pattern)
+    top_level = os.path.realpath(
+        os.path.join(module_dir, "..", "..", "..", "..")
+    )
+    module_tests = loader.discover(module_dir, pattern or "test*.py",
+                                   top_level_dir=top_level)
     return unittest.TestSuite(module_tests)

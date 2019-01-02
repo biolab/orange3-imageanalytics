f1051849ebdae8dcd20a967339d49922fafadbff
Primož Godec <p.godec9@gmail.com>
2019-01-02 17:20:26 +0100

    fix tests trial9

--- orangecontrib/imageanalytics/widgets/tests/test_owimageembedding.py
+++ orangecontrib/imageanalytics/widgets/tests/test_owimageembedding.py
@@ -9,15 +9,11 @@ class DummyCorpus(Table):
 
 
 class TestOWImageEmbedding(WidgetTest):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        cls.signal_name = "Images"
-        cls.signal_data = Table("iris")
 
     def setUp(self):
         self.widget = self.create_widget(OWImageEmbedding)
+        self.signal_name = "Images"
+        self.signal_data = Table("iris")
 
     def test_not_image_data(self):
         """

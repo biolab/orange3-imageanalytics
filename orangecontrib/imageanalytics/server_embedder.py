2576ecb254d3c6d9dcf82b020aaffcc182919df4
Primož Godec <p.godec9@gmail.com>
2020-09-30 14:33:03 +0200

    Image embedding: resize an image before sending to the server

--- orangecontrib/imageanalytics/server_embedder.py
+++ orangecontrib/imageanalytics/server_embedder.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 from Orange.misc.server_embedder import ServerEmbedderCommunicator
 from orangecontrib.imageanalytics.utils.embedder_utils import ImageLoader
@@ -11,12 +11,14 @@ class ServerEmbedder(ServerEmbedderCommunicator):
         max_parallel_requests: int,
         server_url: str,
         embbedder_type: str,
+        image_size: Tuple[int, int],
     ) -> None:
         super().__init__(
             model_name, max_parallel_requests, server_url, embbedder_type
         )
         self.content_type = "image/jpeg"
+        self.image_size = image_size
         self._image_loader = ImageLoader()
 
     async def _encode_data_instance(self, file_path: str) -> Optional[bytes]:
-        return self._image_loader.load_image_bytes(file_path)
+        return self._image_loader.load_image_bytes(file_path, self.image_size)

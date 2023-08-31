034e0e8c77f4e104a26f1561656683549f6f16fe
PrimozGodec <p.godec9@gmail.com>
2023-08-31 11:03:35 +0200

    Remove urllib3 contraint

--- setup.py
+++ setup.py
@@ -101,8 +101,6 @@ if __name__ == '__main__':
             "orange-widget-base >=4.20.0",
             "pillow >=6.2.0",
             "requests",
-            # temporary fixes https://github.com/ionrock/cachecontrol/pull/294
-            "urllib3<2",
             "scipy",
         ],
         extras_require={

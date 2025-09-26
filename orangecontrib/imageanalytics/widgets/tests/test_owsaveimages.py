e3ddc44c009915f1d70fd51b3a62dbe5b1ce140e
Aleš Erjavec <ales.erjavec@fri.uni-lj.si>
2025-09-26 09:32:07 +0200

    test_owsaveimages: Fix test tear down

--- orangecontrib/imageanalytics/widgets/tests/test_owsaveimages.py
+++ orangecontrib/imageanalytics/widgets/tests/test_owsaveimages.py
@@ -23,20 +23,24 @@ LOCAL_IMAGES = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_i
 
 class TestOWSaveImages(WidgetTest):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.data = Table("https://datasets.biolab.si//core/bone-healing.xlsx")
+        # reduce the dataset - take data of both classes
+        cls.data = cls.data[16:22]
+        super().setUpClass()
+
     def setUp(self):
         self.widget = self.create_widget(OWSaveImages)
-        self.data = Table("https://datasets.biolab.si//core/bone-healing.xlsx")
-        # reduce the dataset - take data of both classes
-        self.data = self.data[16:22]
-
         # Create a temporary directory
         self.test_dir = tempfile.mkdtemp()
+        super().setUp()
 
     def tearDown(self):
-        # Close the file, the directory will be removed after the test
         self.widget.cancel()
-        self.widgets.remove(self.widget)
-        self.wait_until_finished()
+        self.widget.onDeleteWidget()
+        super().tearDown()
+        shutil.rmtree(self.test_dir)
 
     def test_dataset(self):
         self.widget.auto_save = True
